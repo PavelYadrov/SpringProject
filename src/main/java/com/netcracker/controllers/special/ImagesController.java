e3977e780a3a81d45d264fbbc3c940dd0cc2ed25
PavelYadrov <iadrovpaul@gmail.com>
2020-07-02 22:16:02 +0300

    Image property

--- src/main/java/com/netcracker/controllers/special/ImagesController.java
+++ src/main/java/com/netcracker/controllers/special/ImagesController.java
@@ -3,16 +3,18 @@ package com.netcracker.controllers.special;
 import com.netcracker.dto.ImageDTO;
 import com.netcracker.services.AdvertisementService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "api/images")
 public class ImagesController {
     private AdvertisementService advertisementService;
+
+    @Value("${server.image-url}")
+    private String url;
+
 
     @Autowired
     public ImagesController(AdvertisementService advertisementService) {
@@ -20,7 +22,12 @@ public class ImagesController {
     }
 
     @PostMapping(value = "addImage")
-    public ResponseEntity<String> addImage(@RequestBody ImageDTO imageDTO){
+    public ResponseEntity<String> addImage(@RequestBody ImageDTO imageDTO) {
         return ResponseEntity.ok(advertisementService.saveImage(imageDTO));
+    }
+
+    @GetMapping(value = "getImageUrl")
+    public ResponseEntity<String> getImageUrl() {
+        return ResponseEntity.ok(url);
     }
 }
